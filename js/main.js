3dd5da5daef78c84b8f6e63839fba5b451b2721d
kattychar <68196780+kattychar@users.noreply.github.com>
2021-01-31 13:53:42 +0300

    Первое задание 2.0

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
 function randomNumber (a, b) {
-  if (a >= b) {
-    //console.log('Параметр а должен быть меньше b');
+  if (a < 0 && b < 0) {
+    alert ('Аргументы должны быть положительные');
   } else {
-    return Math.floor(Math.random() * (b -a + 1)) + a;
+    if (a >= b) {
+      alert('Параметр а должен быть меньше b');
+    } else {
+      return Math.floor(Math.random() * (b -a + 1)) + a;
+    }
   }
 }
-
 randomNumber (3, 10);
+//https://learn.javascript.ru/task/random-int-min-max
+
+function check(str, maxLength) {
+  if(maxLength <= 0) {
+    alert ('Максимальная длина строки введена некорректно, исправьте пожалуйста')
+  } else {
+    if (str.length <= maxLength) {
+      return str;
+    } else {
+      alert('Сообщение превышает максимальную длину в ' + maxLength + ' символов');
+    }
+  }
+}
+check('Hello',5);
+//https://learn.javascript.ru/string
