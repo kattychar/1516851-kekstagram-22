37cb5a7555392319ea7ffe617c9651abf4b1a428
kattychar <68196780+kattychar@users.noreply.github.com>
2021-03-19 00:21:20 +0300

    Сгенерировала коменты пользователей на большой картинке

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,4 +1,3 @@
-//import {similarDescriptions} from './data.js';
 import {generationPictures} from './pictures-miniature.js';
 
 const bigPicture = document.querySelector('.big-picture');
@@ -6,38 +5,47 @@ const img = document.querySelector('.big-picture__img img');
 const likes = document.querySelector('.likes-count');
 const commentsCount = document.querySelector('.comments-count');
 const allComments = document.querySelector('.social__comments');
-//const commentsCountBlock = document.querySelector('.social__comment-count');
-//const commentsLoader = document.querySelector('.comments-loader');
+const commentsCountBlock = document.querySelector('.social__comment-count');
+const commentsLoader = document.querySelector('.comments-loader');
 const picDescription = document.querySelector('.social__caption');
-//const body = document.querySelector('body');
+const body = document.querySelector('body');
 const commentTemplate = document.querySelector('#comment')
   .content
   .querySelector('.social__comment');
-//const cancelButton = document.querySelector('.big-picture__cancel');
-
-bigPicture.classList.remove('hidden');
+const cancelButton = document.querySelector('.big-picture__cancel');
 
 document.querySelector('.social__comments').innerHTML = '';
 
-const bigPictureData = generationPictures[5];
-img.src = bigPictureData.url;
-likes.textContent = bigPictureData.likes;
-commentsCount.textContent = bigPictureData.comments.length;
-picDescription.textContent = bigPictureData.description;
-
-const commentFragment = document.createDocumentFragment();
-
-generationPictures.forEach(({avatar, name, message}) => {
+function makeComment(commentData) {
   let comment = commentTemplate.cloneNode(true);
-  comment.querySelector('.social__picture').src = avatar;
-  comment.querySelector('.social__picture').alt = name;
-  comment.querySelector('.social__text').textContent = message;
-  commentFragment.appendChild(comment);
-})
+  comment.querySelector('.social__picture').src = commentData.avatar;
+  comment.querySelector('.social__picture').alt = commentData.name;
+  comment.querySelector('.social__text').textContent = commentData.message;
+  return comment;
+}
 
-allComments.appendChild(commentFragment);
+function makeComments (comments) {
+  const commentFragment = document.createDocumentFragment();
 
-/*function openBigPicture () {
+  comments.forEach(element => {
+    commentFragment.appendChild(makeComment(element))
+  });
+
+  allComments.appendChild(commentFragment);
+}
+
+for (let i = 0; i < generationPictures.length; i++) {
+  const bigPictureData = generationPictures[i];
+  img.src = bigPictureData.url;
+  likes.textContent = bigPictureData.likes;
+  commentsCount.textContent = bigPictureData.comments.length;
+  picDescription.textContent = bigPictureData.description;
+  if (bigPictureData.comments.length > 0) {
+    makeComments(bigPictureData.comments);
+  }
+}
+
+function openBigPicture () {
   bigPicture.classList.remove('hidden');
   commentsCountBlock.classList.add('hidden');
   commentsLoader.classList.add('hidden');
@@ -48,7 +56,3 @@ function closeBigPicture () {
   bigPicture.classList.add ('hidden');
   body.classList.remove('.modal-open');
 }
-
-cancelButton.addEventListener('click', function() {
-  closeBigPicture();
-})*/
